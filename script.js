3ffa3515228f2f8971ab83e3fd07c4c06b0eeb32
Roman <arom0808@mail.ru>
2021-02-22 18:25:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function onLoad(){
+    onProductsResize();
+    window.addEventListener("resize", onProductsResize);
+}
+function onProductsResize(){
+    let products=document.getElementsByClassName("products-container").item(0);
+    let productsRowWidth=parseInt(products.clientWidth*0.8/150);
+    if(productsRowWidth==0)
+        productsRowWidth=1;
+    console.log(productsRowWidth);
+    products.style.gridTemplateColumns="repeat("+productsRowWidth.toString()+",1fr)"
+}
