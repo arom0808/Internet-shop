0422d05d5c867373d6eeb41e6b8a124890251110
Roman <arom0808@mail.ru>
2021-02-23 10:58:00 +0300

    first page done

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function onLoad(){
 function onProductsResize(){
     let products=document.getElementsByClassName("products-container").item(0);
     let productsRowWidth=parseInt(products.clientWidth*0.8/150);
-    if(productsRowWidth==0)
+    if(productsRowWidth===0)
         productsRowWidth=1;
     products.style.gridTemplateColumns="repeat("+productsRowWidth.toString()+",1fr)"
 }
